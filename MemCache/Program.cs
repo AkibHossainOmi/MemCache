9fbc993e928aa3b1c512dbb727c4ae53ca1f8fb1
AkibHossainOmi <akibhossainomi2000@gmail.com>
2025-01-28 14:00:18 +0600

    RedCache using Redis added.

--- MemCache/Program.cs
+++ MemCache/Program.cs
@@ -6,11 +6,32 @@ namespace MemCache
     {
         static void Main(string[] args)
         {
-            InMemoryCache();
+            RedCache();
         }
         public static void InMemoryCache()
         {
             var cache = new InMemoryCache();
+
+            // Adding entries to the cache
+            cache.Add("470011775763494", DateTime.Parse("2025-01-08 06:04:22"), "8801775763494");
+            cache.Add("470011775763495", DateTime.Parse("2025-01-08 06:05:22"), "8801775763495");
+
+            // Retrieving an entry
+            string phoneNumber = cache.Get("470011775763494", DateTime.Parse("2025-01-08 06:04:22"));
+            Console.WriteLine($"Retrieved Phone Number: {phoneNumber}");
+
+            // Removing an entry
+            cache.Remove("470011775763494", DateTime.Parse("2025-01-08 06:04:22"));
+
+            // Display all entries
+            cache.DisplayAll();
+        }
+
+        public static void RedCache()
+        {
+            var connectionString = "localhost:6379";
+
+            var cache = new RedCache(connectionString);
 
             // Adding entries to the cache
             cache.Add("470011775763494", DateTime.Parse("2025-01-08 06:04:22"), "8801775763494");
---
+++ MemCache/RedCache.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace MemCache
+{
+    public class RedCache
+    {
+        private IDatabase _cache;
+        private ConnectionMultiplexer _redis;
+
+        public RedCache(string redisConnectionString)
+        {
+            _redis = ConnectionMultiplexer.Connect(redisConnectionString);
+            _cache = _redis.GetDatabase();
+        }
+
+        // Method to add a key-value pair to Redis
+        public void Add(string imsi, DateTime startTime, string phoneNumber)
+        {
+            var key = new CacheKey(imsi, startTime);
+            var redisKey = key.ToString(); // Use the string representation of CacheKey as the Redis key
+
+            if (!_cache.KeyExists(redisKey))
+            {
+                _cache.StringSet(redisKey, phoneNumber);
+                Console.WriteLine($"Added to Redis: {redisKey} => {phoneNumber}");
+            }
+            else
+            {
+                Console.WriteLine($"Key {redisKey} already exists in Redis.");
+            }
+        }
+
+        // Method to retrieve a value by key from Redis
+        public string Get(string imsi, DateTime startTime)
+        {
+            var key = new CacheKey(imsi, startTime);
+            var redisKey = key.ToString();
+
+            var phoneNumber = _cache.StringGet(redisKey);
+            if (phoneNumber.HasValue)
+            {
+                return phoneNumber.ToString();
+            }
+            else
+            {
+                Console.WriteLine($"Key {redisKey} not found in Redis.");
+                return null;
+            }
+        }
+
+        // Method to remove a key-value pair from Redis
+        public void Remove(string imsi, DateTime startTime)
+        {
+            var key = new CacheKey(imsi, startTime);
+            var redisKey = key.ToString();
+
+            if (_cache.KeyExists(redisKey))
+            {
+                _cache.KeyDelete(redisKey);
+                Console.WriteLine($"Removed from Redis: {redisKey}");
+            }
+            else
+            {
+                Console.WriteLine($"Key {redisKey} not found in Redis.");
+            }
+        }
+
+        // Method to display all key-value pairs in Redis
+        public void DisplayAll()
+        {
+            Console.WriteLine("\nCurrent Redis Cache:");
+            var endpoints = _redis.GetEndPoints();
+            var server = _redis.GetServer(endpoints[0]);
+
+            // Get all keys (for small data, this works fine)
+            foreach (var key in server.Keys(pattern: "*"))
+            {
+                var value = _cache.StringGet(key);
+                Console.WriteLine($"{key} => {value}");
+            }
+        }
+    }
+}
