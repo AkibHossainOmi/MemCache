c7583205b4c171353528fd573e292a9adae2b978
AkibHossainOmi <akibhossainomi2000@gmail.com>
2025-01-30 14:29:52 +0600

    GarnetCache added

--- MemCache/Program.cs
+++ MemCache/Program.cs
@@ -6,7 +6,7 @@ namespace MemCache
     {
         static void Main(string[] args)
         {
-            RedCache();
+            GarnetCache();
         }
         public static void InMemoryCache()
         {
@@ -28,6 +28,27 @@ namespace MemCache
         }
 
         public static void RedCache()
+        {
+            var connectionString = "localhost:6379";
+
+            var cache = new RedCache(connectionString);
+
+            // Adding entries to the cache
+            cache.Add("470011775763494", DateTime.Parse("2025-01-08 06:04:22"), "8801775763494");
+            cache.Add("470011775763495", DateTime.Parse("2025-01-08 06:05:22"), "8801775763495");
+
+            // Retrieving an entry
+            string phoneNumber = cache.Get("470011775763494", DateTime.Parse("2025-01-08 06:04:22"));
+            Console.WriteLine($"Retrieved Phone Number: {phoneNumber}");
+
+            // Removing an entry
+            cache.Remove("470011775763494", DateTime.Parse("2025-01-08 06:04:22"));
+
+            // Display all entries
+            cache.DisplayAll();
+        }
+
+        public static void GarnetCache()
         {
             var connectionString = "localhost:6379";
 
